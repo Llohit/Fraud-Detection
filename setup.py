62ee1c0ee3974f96db9c8fe608181406c9a58d29
Meet Mandhane <51969609+meetmandhanemnm@users.noreply.github.com>
2022-12-12 20:12:34 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='numpy',
+      version='1.21.6',
+      description='Scientific computing in python',
+     )
+
+setup(name='pandas',
+      version='1.3.5',
+      description='Data analysis and manipulation',
+     )
+
+setup(name='matplotlib',
+      version='3.5.3',
+      description='creating visualizations in python',
+     )
+
+setup(name='seaborn',
+      version='0.11.2',
+      description='Data visualzation built on top of matplotlib',
+     )
