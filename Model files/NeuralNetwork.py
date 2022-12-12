73e82ccdaa1d316d33c3351a6c66d5e9b9d3875a
Meet Mandhane <51969609+meetmandhanemnm@users.noreply.github.com>
2022-12-12 19:14:54 +0530

    Update NeuralNetwork.py

--- Model files/NeuralNetwork.py
+++ Model files/NeuralNetwork.py
@@ -7,23 +7,13 @@ Original file is located at
     https://colab.research.google.com/drive/1nH35Lu5MAEf2DKycUf_ljpiMoLkJA1dQ
 """
 
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 
 newdf=pd.read_csv('../input/fraudaa/newdf2.csv')
 
@@ -37,8 +27,6 @@ ar1=[]
 for i in newdf.columns:
     if(i in ar):
         ar1.append(i)
-
-print(len(ar1))
 
 for i in newdf.columns:
     if(i in ar1):
@@ -144,7 +132,7 @@ model.fit(X_train,y_train,class_weight=weights_assigned,epochs=10)
 
 y_pred1=model.predict(X_test)
 
-y_pred1
+print(y_pred1)
 
 c1=0
 c0=0
@@ -170,5 +158,5 @@ for i in y_pred1:
         a.append(0)
 Counter(a)
 
-y_pred1
+print(y_pred1)
 
